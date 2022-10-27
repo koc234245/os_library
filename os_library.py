4088f838de872e271d19a160e379d556e83ddd98
koc234245 <kocrys@gmail.com>
2022-10-27 16:18:40 +0600

    bugfix

--- os_library.py
+++ os_library.py
@@ -1,11 +1,16 @@
 import os
 
 for dirpath, dirnames, filenames in os.walk("papka"):
-    for dirname in dirnames:
-        os.removedirs(os.path.join(dirpath, dirname))
     for filename in filenames:
         os.remove(os.path.join(dirpath, filename))
-os.makedirs ("papka/papka_v_papke/")
-os.chdir("papka/papka_v_papke")
-text_file = open("text.txt", "w")
-text_file.write("здесь был космодесант here was space marines")
+        os.removedirs(os.path.join(dirpath))
+
+def make_file():
+    os.makedirs ("papka/papka_v_papke/")
+    os.chdir("papka/papka_v_papke")
+    text_file = open("text.txt", "w")
+    text_file.write("здесь был космодесант here was space marines")
+    
+make_file()
+
+    
