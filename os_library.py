611498df8d77497004da083ebebc6894a4d03503
koc234245 <kocrys@gmail.com>
2022-10-27 17:04:48 +0600

    full work

--- os_library.py
+++ os_library.py
@@ -1,16 +1,28 @@
 import os
 
-for dirpath, dirnames, filenames in os.walk("papka"):
-    for filename in filenames:
-        os.remove(os.path.join(dirpath, filename))
-        os.removedirs(os.path.join(dirpath))
-
 def make_file():
-    os.makedirs ("papka/papka_v_papke/")
-    os.chdir("papka/papka_v_papke")
-    text_file = open("text.txt", "w")
-    text_file.write("здесь был космодесант here was space marines")
-    
-make_file()
+    user_folder = input("введите имя первой папки: ")
+    user_folder2 = input("введите имя второй папки: ")
+    user_file = input("введите имя файла: ") + ".txt"
+
+    for dirpath, dirnames, filenames in os.walk(user_folder):
+        for filename in filenames:
+          os.remove(os.path.join(dirpath, filename))
+          os.removedirs(os.path.join(dirpath))
+
+    os.makedirs(user_folder + "/" + user_folder2)
+    os.chdir(user_folder + "/" + user_folder2)
+    text_file = open(user_file, "w")
+    in_text_message = input("что написать в файле?: ")
+    text_file.write(in_text_message)
+
+confirmation = input("желаете создать файл? Введите 'да' или 'нет':" )
+match confirmation:
+    case "да": 
+        make_file()
+    case "нет":
+        print("хорошо")
+    case _:
+        print("Упс, вы что-то не то написали")
 
     
