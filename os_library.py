fdbb4ab90c2767081f03acfda597586f0616a7da
koc234245 <kocrys@gmail.com>
2022-10-27 15:53:58 +0600

    deleted if

--- os_library.py
+++ os_library.py
@@ -1,11 +1,11 @@
 import os
-if os.path.isdir("papka"):
-    os.remove("papka/papka_v_papke/text.txt")
-    os.removedirs("papka/papka_v_papke")
 
-if not os.path.isdir("papka"):
-    os.mkdir ("papka")
-    os.mkdir ("papka/papka_v_papke")
-    os.chdir("papka/papka_v_papke")
-    text_file = open("text.txt", "w")
-    text_file.write("здесь был космодесант here was space marines")
+for dirpath, dirnames, filenames in os.walk("papka"):
+    for dirname in dirnames:
+        os.removedirs(os.path.join(dirpath, dirname))
+    for filename in filenames:
+        os.remove(os.path.join(dirpath, filename))
+os.makedirs ("papka/papka_v_papke/")
+os.chdir("papka/papka_v_papke")
+text_file = open("text.txt", "w")
+text_file.write("здесь был космодесант here was space marines")
